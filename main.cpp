99b4e445f5471d45bbc259b83f3d6e352c793d29
Alessandro Pintus <apintus@made-f0Ar5s11.clusters.42paris.fr>
2025-03-25 18:37:14 +0100

    ajouut getter setter Config et dossier test parsing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include "srcs/LogManager/LogManager.hpp"
+#include "Config.hpp"
 
-int main(int /*ac*/, char **/*av*/)
+int main(int ac, char **av)
 {
-    // if (ac != 2) 
-    // {
-    //     std::cerr << "Usage:./webserv [configuration file] " << std::endl;
-    //     return (EXIT_FAILURE);
-    // }
-
-    //ignorer **av[1] pour le moment
-
+    // A enlever plus tard car config par default acceptable si pas de file
+    if (ac != 2) 
+    {
+        std::cerr << "Usage:./webserv [configuration file] " << std::endl;
+        return (EXIT_FAILURE);
+    }
+    //parsing fichier config (validation et recuperation des données)
+    try {
+        Config config;
+        config.parseConfigFile(av[1], config);
+        LogManager::log(LogManager::INFO, "Fichier config correct!");
+    }
+    catch (const std::exception &e) {
+         LogManager::log(LogManager::ERROR, e.what());
+        return (EXIT_FAILURE);
+    }
+    
     LogManager::setLogStatus(true);
     LogManager::setLogFileStatus(true);
     LogManager::setLogConsoleStatus(true);
 
     LogManager::log(LogManager::DEBUG, "Server is starting...");
     
-    //parsing fichier config (validation et recuperation des données)
-
     //creation du serveur : socket et de epoll pour surveiler les sockets
         // gestion client (creer/ajouter nouveau socket a epoll)
 
---
+++ srcs/Config/BlocLocation.hpp
@@ -0,0 +1,48 @@
+#ifndef BLOCLOCATION_HPP
+# define BLOCLOCATION_HPP
+
+# include "BlocServer.hpp"
+
+class BlocLocation
+{
+    private:
+            std::string                 _root;
+            std::string                 _alias;
+            std::vector<std::string>    _index;
+            std::set<std::string>       _allowMethod; //une seul clef mais plusieur valeur en face
+            //std::vector<std::string>    _cgiExtension; Bonus a voir plus tard
+            std::string                 _uploadPath;
+            bool                        _autoIndex;
+            
+    public:
+            BlocLocation();
+            BlocLocation(const BlocLocation &copy);
+            ~BlocLocation();
+
+            BlocLocation &operator=(const BlocLocation &assign);
+
+            //Setters
+            void    setRoot(const std::string &root) {_root = root;};
+            void    setAlias(const std::string &alias) {_alias = alias;};
+            void    addIndex(const std::string &index) {_index.push_back(index);};
+            void    addAllowMethod(const std::string &method) {_allowMethod.insert(method);};
+            //void    addCgiExtension(const std::string &extension); Bonus a voir plus tard
+            void    setUploadPath(const std::string &path) {_uploadPath = path;};
+            void    setAutoIndex(bool autoIndex) { _autoIndex = autoIndex;};
+
+            //Getters
+            const std::string                   &getRoot() const {return _root;};
+            const std::string                   &getAlias() const {return _alias;};
+            const std::vector<std::string>      &getIndex() const {return _index;};
+            const std::set<std::string>         &getAllowMethod() const {return _allowMethod;};
+            //const std::vector<std::string> &getCgiExtension() const; Bonus a voir plus tard
+            const std::string                   &getUploadPath() const {return _uploadPath;};
+            bool                                getAutoIndex() const {return _autoIndex;};
+
+            //Validation
+            bool    validateLocation() const;
+
+            //Print
+};
+
+#endif
