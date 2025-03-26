2769021f5e630ef84f77e76c70c825e4208387a5
Karim Ammi <karim.ammi@hotmail.com>
2025-03-26 18:24:18 +0100

    recuperation config

---
+++ srcs/Utils/Utils.hpp
@@ -0,0 +1,14 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <iostream>
+# include <sstream>
+
+
+class Utils
+{
+    public:
+        static int ft_stoi(const std::string &input);
+};
+
+#endif
