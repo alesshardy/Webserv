e1e6d47eb9af82678a0947b05dac7614abe37fa2
Alessandro Pintus <apintus@made-f0Ar5s11.clusters.42paris.fr>
2025-03-28 18:01:35 +0100

    ajout de arg a la class qui avance

--- srcs/Config/Config.cpp
+++ srcs/Config/Config.cpp
@@ -101,6 +101,7 @@ std::string Config::findFirstWord(std::string line)
 //     }
 // }
 
+// VERIF KEY
 bool    Config::verifKeyServer(std::string token)
 {
     if (token == "server")
@@ -117,8 +118,24 @@ bool    Config::verifKeyOther(std::string token)
     return false;
 }
 
+//VERIF ARG
+
+/**
+ * @brief verifie si un chemin donne est un repertoire valid sur le systeme de fichier
+ * 
+ * @param path 
+ * @return true 
+ * @return false 
+ */
+bool        Config::isValidRoot(const std::string &path)
+{
+    struct stat info;
+    return (stat(path.c_str(), &info) == 0 && S_ISDIR(info.st_mode));
+}
+
+
 //TEST AJOUT SIMPLE SANS PARSER
-void        Config::addArgToServerBloc(std::string arg, std::string lastKey, BlocServer &current)
+void        Config::addArgToServerBloc(std::string arg, std::string lastKey, BlocServer &current, int argNb)
 {
     if (lastKey == "listen")
     {
@@ -129,32 +146,65 @@ void        Config::addArgToServerBloc(std::string arg, std::string lastKey, Blo
         current.addServerName(arg);
     }
     else if (lastKey == "root")
-    {   
+    {
+        if (argNb > 1)
+            throw std::runtime_error("ERROR : too many argument for root key");
         if (!current.getRoot().empty())
-            throw std::runtime_error("ERREUR : double root dans bloc server ");    
+            throw std::runtime_error("ERROR : double root in server bloc");
+        if (!isValidRoot(arg))
+            throw std::runtime_error("ERROR : root must be an absolute path");   
         current.setRoot(arg);
     }
     else if (lastKey == "index")
     {
         current.addIndex(arg);
     }
-    // bourbier quand plusieur element sur meme ligne
-    // if (lastKey == "error_page")
-    // {
-    //     current.addErrorPage(arg);
-    // }
+    //choisi d'ecraser si plusieur code erreur car nginx accepte doublon
+    else if (lastKey == "error_page")
+    {
+        if (argNb == 1)
+        {
+            int code ;
+            try {
+                code = Utils::ft_stoi(arg);
+            }
+            catch (const std::exception &e){
+                throw std::runtime_error("ERROR : wrong error_page code");   
+            }
+            if (code >= 300 && code <= 599)
+                current.setTmpErrorCode(code);
+            else
+                throw std::runtime_error("ERROR : wrong error_page code must be between 300 && 599");   
+        }
+        else if (argNb == 2)
+        {
+            if (!current.hasTmpErrorCode())
+                throw std::runtime_error("ERROR : no code define before arg");
+            int code = current.getTmpErrorCode();
+            current.addErrorPage(code, arg);
+            current.clearTmpErrorCode();
+        }
+        else
+            throw std::runtime_error("ERROR : too many arg for error_page");   
+    }
     else if (lastKey == "client_max_body_size")
     {
         current.setClientMaxBodySize(Utils::ft_stolonglong(arg));
     }
     else
-        throw std::runtime_error("ERREUR : pas la bonne cle pour le bloc serveur ");    
+        throw std::runtime_error("ERROR : bad key for server bloc ");    
 }
 
-void        Config::addArgToLocationBloc(std::string arg, std::string lastKey, BlocLocation &current)
+void        Config::addArgToLocationBloc(std::string arg, std::string lastKey, BlocLocation &current, int argNb)
 {
     if (lastKey == "root")
     {
+        if (argNb > 1)
+            throw std::runtime_error("ERROR : too many argument for root key");
+        if (!current.getRoot().empty())
+            throw std::runtime_error("ERROR : double root in location bloc");
+        if (!isValidRoot(arg))
+            throw std::runtime_error("ERROR : root must be an absolute path");
         current.setRoot(arg);
     }
     else if (lastKey == "alias")
@@ -167,31 +217,41 @@ void        Config::addArgToLocationBloc(std::string arg, std::string lastKey, B
     }
     else if (lastKey == "allow_methods")
     {
-        current.addAllowMethod(arg);
+        const std::set<std::string> &methods = current.getAllowMethod();
+        if (methods.find(arg) != methods.end())
+            throw std::runtime_error("ERROR : Method '" + arg + "' already define");
+        if (arg == "GET" || arg == "POST" || arg == "DELETE")
+            current.addAllowMethod(arg);
+        else
+            throw std::runtime_error("ERROR : bad argument for allow_methods key");
     }
     else if (lastKey == "upload_path")
     {
         current.setUploadPath(arg);
     }
     else if (lastKey == "autoindex")
-    {
-        if (arg == "true")
+    {   
+        if (argNb > 1)
+            throw std::runtime_error("ERROR :too many argument for autoindex key");
+        if (arg == "on")
             current.setAutoIndex(true);
-        current.setAutoIndex(false);
+        else if (arg == "off")
+            current.setAutoIndex(false);
+        else 
+            throw std::runtime_error("ERROR : bad argument for autoindex key");
     }
     else
-        throw std::runtime_error("ERREUR : pas la bonne cle pour le bloc location ");
+        throw std::runtime_error("ERROR : bad key for location bloc");
 }
 
-
-
+//A decouper fonction principale pour verifier que le fichier config est correct
 void    Config::parseConfigFile(const std::string &filePath, Config &config) 
 {
     (void)config;
     std::ifstream file(filePath.c_str());
     if (!file) 
     {
-        throw std::runtime_error("Erreur : impossible d'ouvrir le fichier ");
+        throw std::runtime_error("ERROR : impossible to open file");
         return;
     }
 
@@ -211,6 +271,7 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
     BlocLocation    currentLocation;
     BlocServer      currentServer;
     std::string     currentLocationPath;
+    int    argNb = 0;
 
     while (file.get(c)) 
     {
@@ -243,28 +304,30 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
                     std::cout << "Clé trouvé1 : " << token << std::endl;
                     lastKey = token;
                     if (!verifKeyServer(token))
-                        throw std::runtime_error("ERREUR : first key must be 'server' ");
+                        throw std::runtime_error("ERROR : first key must be 'server' ");
                     currentServer = BlocServer();
                 } 
                 else 
                 {
                     std::cout << "Argument trouvé1 : " << token << std::endl;
+                    argNb++;
                 }
                 // requete doit finir par ';' partie 1
                 if (semicolonCount != 1 && token != "server")
                 {
-                    throw std::runtime_error("ERREUR : ligne requete doit finir par ';' OU premier bloc doit etre server ");
+                    throw std::runtime_error("ERROR : line request must end by ';' OR first bloc must be server ");
                 }
                 token.clear(); // Vider le token à la fin de la ligne
             }
             // REGLER CE PUTIN DE PB D"ESPACE DE CON
             if (semicolonCount != 1 && lastKey != "server" && lastKey != "location" && lastC != '\n' && lastC != '}')
             {
-                throw std::runtime_error("ERREUR : SVPPPP");
+                throw std::runtime_error("ERROR : line request must end by ';'");
             }
-                        semicolonCount = 0; // Réinitialiser le compteur pour la nouvelle ligne
+            semicolonCount = 0; // Réinitialiser le compteur pour la nouvelle ligne
             contentAfterSemicolon = false;
             isKey = true; // Réinitialiser pour la nouvelle ligne
+            argNb = 0;
             lastC = c;
         } 
         // Gestion des blocs
@@ -275,9 +338,9 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
             isKey = true; // Réinitialiser pour le début d'un bloc
             emptyBloc = true;
             if (depth == 2 && inLocBloc == false)
-                throw std::runtime_error("Erreur : bloc de lvl 2 doit etre location") ;
+                throw std::runtime_error("ERROR : bloc of lvl 2 must be location") ;
             if (depth == 3)
-                throw std::runtime_error("Erreur : j'accepte pas les locations imbriquer #inception") ;
+                throw std::runtime_error("ERROR : we don't accept location in location were are not in inception") ;
             lastC = c;
         } 
         else if (c == '}') 
@@ -286,7 +349,7 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
             // gerer les blocs vides
             if (emptyBloc)
             {
-                throw std::runtime_error("Erreur : le fichier ne peux contenir de bloc vide") ;
+                throw std::runtime_error("ERROR : file can't have empty bloc") ;
             }
             // AJOUT DES BLOC DE CLASS
             if (depth == 2)
@@ -309,10 +372,10 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
         {
             semicolonCount++;
             if (depth == 0)
-                throw std::runtime_error("Erreur : directive 'server' has no opening '{'");
+                throw std::runtime_error("ERROR : directive 'server' has no opening '{'");
             if (semicolonCount > 1) 
             {
-                throw std::runtime_error("Erreur : plus d'une requête par ligne détectée.");
+                throw std::runtime_error("ERROR : multiple request in one line");
             }
             if (!token.empty()) 
             {
@@ -325,16 +388,17 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
                 {
                     std::cout << "Argument trouvé2 : " << token << std::endl;
                     //AJOUT AU CLASS
+                    argNb++;
                     if (depth == 1 && lastKey != "location")
-                         addArgToServerBloc(token, lastKey, currentServer);
+                         addArgToServerBloc(token, lastKey, currentServer, argNb);
                      else if (depth == 2)
-                         addArgToLocationBloc(token, lastKey, currentLocation);
+                         addArgToLocationBloc(token, lastKey, currentLocation, argNb);
                 }
                 token.clear(); // Vider le token après un point-virgule
             }
             else if (isKey == true)
             {
-                throw std::runtime_error("Erreur : unexpected ';' ");
+                throw std::runtime_error("ERROR : unexpected ';' ");
             }
             isKey = true; // Réinitialiser pour que le prochain mot soit une clé
             lastC = c;
@@ -342,7 +406,7 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
         // requete doit finir par ';' partie 2
         else if (semicolonCount == 1 && !isspace(c) && c != '\n')
         {
-            throw std::runtime_error("Erreur : ligne requete doit finir par ';2' ");
+            throw std::runtime_error("ERROR : line request must end by ';' ");
         }
         // Gestion des espaces
         else if (isspace(c)) 
@@ -360,7 +424,7 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
                         inLocLine = true;
                     }
                     if (!verifKeyOther(token))
-                        throw std::runtime_error("ERREUR : key unknown ");
+                        throw std::runtime_error("ERROR : key unknown ");
                     
                 } 
                 else 
@@ -368,15 +432,16 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
                     std::cout << "Argument trouvé3 : " << token << std::endl;
                     inLocLine = false;
                     //AJOUT AU CLASS
+                    argNb++;
                     if (depth == 1 && lastKey != "location")
-                        addArgToServerBloc(token, lastKey, currentServer);
+                        addArgToServerBloc(token, lastKey, currentServer, argNb);
                     else if (depth == 1 && lastKey == "location")
                     {
                         currentLocation = BlocLocation();
                         currentLocationPath = token;
                     }
                     else if (depth == 2)
-                        addArgToLocationBloc(token, lastKey, currentLocation);
+                        addArgToLocationBloc(token, lastKey, currentLocation, argNb);
                 }
                 token.clear();
             }
@@ -390,7 +455,7 @@ void    Config::parseConfigFile(const std::string &filePath, Config &config)
         }
     }
     if (depth != 0)
-        throw std::runtime_error("Erreur : fermeture de bloc incorrect!");
+        throw std::runtime_error("ERROR : a bloc is not close!");
 
     file.close();
 }
