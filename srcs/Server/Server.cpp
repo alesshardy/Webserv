58727f3d378573ebe12861057123d8c5e73a0415
Karim Ammi <karim.ammi@hotmail.com>
2025-04-09 12:44:48 +0200

    fixation segfault request client

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -193,11 +193,13 @@ void Server::handleEpollEvents()
             if (_sockets_map.find(fd) != _sockets_map.end())
             {
                 handleNewConnection(fd);
-                _clients_map[fd]->handleRequest();
+                LogManager::log(LogManager::DEBUG, "New connection on socket %d", fd);
+               
             }
             else
             {
                 handleClientData(fd);
+                _clients_map[fd]->handleRequest();
 
             }
         }
