7baccb3dd82c6e388bd1bb46a900673242f0001b
Alessandro Pintus <apintus@made-f0Ar5s11.clusters.42paris.fr>
2025-04-10 12:59:00 +0200

    a camoufler

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -264,7 +264,7 @@ void Server::handleNewConnection(int socket_fd)
  */
 void Server::handleClientData(int client_fd)
 {
-    char buffer[1] = {0};
+    char buffer[100] = {0};
     int bytes = read(client_fd, buffer, sizeof(buffer));
 
     if (bytes == -1)
