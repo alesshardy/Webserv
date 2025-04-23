288d047fd135da03cdccaf7525ff26fa1de4e1da
Alessandro Pintus <alesshardy22@gmail.com>
2025-04-23 18:49:06 +0200

    faut regler le parsing laaa

--- srcs/Request/CgiRequest.hpp
+++ srcs/Request/CgiRequest.hpp
@@ -3,11 +3,22 @@
 
 #include "Request.hpp"
 
+class Request;
+
 class CgiRequest
 {
+    private:
+            Request *_request;
+            std::map <std::string, std::string> _env;
+           
+            void _initEnv();
+            void _executeCgi();
 
+    public:
+            CgiRequest(Request *request);
+            CgiRequest(CgiRequest const & copy);
+            ~CgiRequest();
 
 };
-
 
 #endif
