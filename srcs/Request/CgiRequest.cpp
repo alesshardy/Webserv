a6493b351e4555633b37bfcf0b948aef4193167a
Alessandro Pintus <apintus@made-f0Ar5s9.clusters.42paris.fr>
2025-04-24 18:31:24 +0200

    quand on va finir les cgi jsp

--- srcs/Request/CgiRequest.cpp
+++ srcs/Request/CgiRequest.cpp
@@ -1,12 +1,75 @@
 #include "CgiRequest.hpp"
 
-CgiRequest::CgiRequest(Request *request): _request(request){_initEnv();}
-
-CgiRequest::CgiRequest(CgiRequest const & copy){*this = copy;}
-
-CgiRequest::~CgiRequest(){};
-
-void    CgiRequest::_initEnv()
+CgiRequest::CgiRequest(Request *request, std::string cgiPath, std::string scriptCgi): _request(request), _cgiPath(cgiPath), _scriptCgi(scriptCgi)
 {
-    _env["SERVER_NAME"] = "Werbserv";
+    _initEnv();
+}
+
+CgiRequest::CgiRequest(CgiRequest const & copy)
+{
+    *this = copy;
+}
+
+CgiRequest::~CgiRequest()
+{
+}
+
+void CgiRequest::_initEnv()
+{  
+    LogManager::log(LogManager::DEBUG, ("Init ENV pour CGI"));
+
+    // Initialiser les variables d'environnement CGI
+    _env["SERVER_NAME"] = "Webserv";
+    _env["SERVER_SOFTWARE"] = "Webserv/1.0";
+    _env["GATEWAY_INTERFACE"] = "CGI/1.1";
+    _env["SERVER_PROTOCOL"] = "HTTP/1.1";
+    _env["REQUEST_METHOD"] = _request->getMethod();
+    _env["SCRIPT_NAME"] = _request->getUri();
+    _env["QUERY_STRING"] = _request->getQueryString().empty() ? "" : _request->getQueryString();
+    _env["CONTENT_TYPE"] = _request->getHeaders().count("Content-Type") ? _request->getHeaders().at("Content-Type") : "";
+    _env["CONTENT_LENGTH"] = _request->getHeaders().count("Content-Length") ? _request->getHeaders().at("Content-Length") : "";
+    _env["HTTP_HOST"] = _request->getHeaders().count("Host") ? _request->getHeaders().at("Host") : "";
+    _env["SERVER_PORT"] = toString(_request->getPort());
+    _env["PATH_INFO"] = _request->getUri();
+    _env["PATH_TRANSLATED"] = _request->getMatchingLocation()->getRoot() + _request->getUri();
+}
+
+void    CgiRequest::_CgiConvertEnvToChar()
+{
+    LogManager::log(LogManager::DEBUG, ("Convert Env to Char[]:"));
+    char    **envp = new char*[_env.size() + 1];
+
+    size_t i= 0;
+    for (std::map<std::string, std::string>::iterator it = _env.begin(); it != _env.end(); ++it)
+    {
+        std::string envStr = it->first + "=" + it->second;
+        envp[i] = strdup(envStr.c_str());
+        i++;
+    }
+
+    envp[i] = NULL;
+    if (LogManager::getDebugLogStatus())
+    {
+        // Debug : Afficher les variables d'environnement converties
+        for (size_t j = 0; j < i; ++j)
+            std::cout << envp[j] << std::endl;
+    }
+}
+void    CgiRequest::executeCgi()
+{
+    if (LogManager::getDebugLogStatus())
+        _printEnv();
+        
+    _CgiConvertEnvToChar();
+
+}
+
+// print env
+void   CgiRequest::_printEnv()
+{
+    LogManager::log(LogManager::DEBUG, ("Print ENV:"));
+    for (std::map<std::string, std::string>::iterator it = _env.begin(); it != _env.end(); ++it)
+    {
+        std::cout << it->first << " = " << it->second << std::endl;
+    }
 }
--- srcs/Request/CgiRequest.hpp
+++ srcs/Request/CgiRequest.hpp
@@ -10,15 +10,24 @@ class CgiRequest
     private:
             Request *_request;
             std::map <std::string, std::string> _env;
-           
-            void _initEnv();
-            void _executeCgi();
+            char**      _envp;
+            std::string _cgiPath;
+            std::string _scriptCgi;
+            char **args[3];
+            pid_t _pid;
+            int _stdin;
+            int _stdout;
 
+            void _initEnv();
+            void _CgiConvertEnvToChar();
+            void _printEnv();
+            
     public:
-            CgiRequest(Request *request);
+            CgiRequest(Request *request, std::string cgiPath, std::string scriptCgi);
             CgiRequest(CgiRequest const & copy);
             ~CgiRequest();
-
+            
+            void executeCgi();
 };
 
 #endif
