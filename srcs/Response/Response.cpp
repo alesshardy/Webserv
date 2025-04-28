f6d705bfd41a3dbb1d0d11c67e45e6d3bdc9a3ac
Karim Ammi <kammi@bess-f3r5s3.clusters.42paris.fr>
2025-04-28 18:28:22 +0200

    aaaaaaaaaaa

--- srcs/Response/Response.cpp
+++ srcs/Response/Response.cpp
@@ -836,6 +836,6 @@ std::string getContentType(const std::string &filePath)
 bool Response::isTimeoutExceeded() const
 {
     std::time_t now = std::time(NULL);
-    return (now - _timeOut > 10); // SIUU CHANGER EN 60 sec
+    return (now - _timeOut > 60); // SIUU CHANGER EN 60 sec
 }
 
