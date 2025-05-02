de82010a0bd6e8eabe6044227e6ad07c207ac0f9
Karim Ammi <kammi@bess-f3r5s3.clusters.42paris.fr>
2025-05-02 13:31:44 +0200

    codes d'erreurs

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -62,7 +62,8 @@ void Client::handleRequest(std::string const & str)
         LogManager::log(LogManager::ERROR, "Error handling request for client %d: \n%s", _client_fd, e.what());
         //SIUUU Gérer l'erreur (fermer le client, envoyer une réponse d'erreur, etc.)
         _request->setState(ERROR);
-        _request->setCode(400);     
+        if (_request->getStatusCode() == 200)
+            _request->setCode(400);     
     }
 }
 
