a6746275afc98f17afab3783f5661b4291cf999c
Karim Ammi <karim.ammi@hotmail.com>
2025-05-02 18:14:04 +0200

    CommandLineParser correction

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -27,55 +27,73 @@ int main(int ac, char **av)
     signal(SIGTERM, signalHandler);
 
     //Initialisation du gestionnaire d'arguments de la ligne de commande
-    CommandLineParser cmdParser(ac, av);
-    LogManager::log(LogManager::DEBUG, "CommandLineParser initialized");
-
-    if (cmdParser.hasOption("help"))
-    {
-        cmdParser.printHelp();
-        return (EXIT_SUCCESS);
-    }
-
     try
     {
-        Config config;
-        
-        std::string configFile = cmdParser.getConfigFilePath();
-        LogManager::log(LogManager::INFO, "Using configuration file: %s", configFile.c_str());
-        config.parseConfigFile(configFile, config);
+        CommandLineParser cmdParser(ac, av);
 
-        if (LogManager::getDebugLogStatus())
-            config.printConfig();
-        
-        // Initialiser le serveur
-        Server server;
-        server.set_config(config); // Assigner la configuration au serveur
-        server.init();             // Initialiser le serveur
-
-        LogManager::log(LogManager::INFO, "Server initialized successfully %d", server.get_socket());
-
-        // Lancer le serveur
-        server.run();
-
-        // Arrêter le serveur
-        server.stop();
-       
-        // Vérifiez si le programme doit quitter
-        if (!running)
+        LogManager::log(LogManager::DEBUG, "CommandLineParser initialized");
+    
+        if (cmdParser.hasOption("help"))
         {
-            LogManager::log(LogManager::INFO, "Exiting program...");
-            return EXIT_SUCCESS;
+            cmdParser.printHelp();
+            return (EXIT_SUCCESS);
         }
+    
+        try
+        {
+            Config config;
+            
+            std::string configFile = cmdParser.getConfigFilePath();
+            LogManager::log(LogManager::INFO, "Using configuration file: %s", configFile.c_str());
+            config.parseConfigFile(configFile, config);
+    
+            if (LogManager::getDebugLogStatus())
+                config.printConfig();
+            
+            // Initialiser le serveur
+            Server server;
+            server.set_config(config); // Assigner la configuration au serveur
+            server.init();             // Initialiser le serveur
+    
+            LogManager::log(LogManager::INFO, "Server initialized successfully %d", server.get_socket());
+    
+            // Lancer le serveur
+            server.run();
+    
+            // Arrêter le serveur
+            server.stop();
+           
+            // Vérifiez si le programme doit quitter
+            if (!running)
+            {
+                LogManager::log(LogManager::INFO, "Exiting program...");
+                return EXIT_SUCCESS;
+            }
+        }
+        catch (const std::exception &e)
+        {
+            LogManager::log(LogManager::ERROR, e.what());
+            return EXIT_FAILURE;
+        }
+        //fermeture du serveur
+        LogManager::log(LogManager::INFO, "Server is shutting down...");
+        LogManager::cleanup();
+        return (EXIT_SUCCESS);
     }
     catch (const std::exception &e)
     {
-        LogManager::log(LogManager::ERROR, e.what());
-        return EXIT_FAILURE;
+        LogManager::log(LogManager::ERROR, "Error: %s", e.what());
+        return (EXIT_FAILURE);
     }
-
-    //fermeture du serveur
-    LogManager::log(LogManager::INFO, "Server is shutting down...");
+    catch (...)
+    {
+        LogManager::log(LogManager::ERROR, "Unknown error occurred");
+        return (EXIT_FAILURE);
+    }
+    LogManager::log(LogManager::INFO, "Exiting program...");
     LogManager::cleanup();
     return (EXIT_SUCCESS);
+
+
 }
 
